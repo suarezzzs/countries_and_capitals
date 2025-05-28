876cd3452a2cdcf7e9097f5c350e931d7dc5ef50
Suárez <carmaso_@hotmail.com>
2025-05-27 22:21:32 -0300

    feat: add controllers, routes, and add_data.php script

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello, World!";
 });
+
+Route::get("show_data", [MainController::class, 'showData'])
+    ->name('show_data');
